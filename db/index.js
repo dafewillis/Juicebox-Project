b8d98f2c21574e4120811b568363a9ebce02129b
David Willis <dafewillis@gmail.com>
2022-03-19 14:28:09 -0600

    millionth test commit

--- db/index.js
+++ db/index.js
@@ -1,5 +1,4 @@
 const { Client } = require('pg');
-
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
 async function createUser({
@@ -73,6 +72,20 @@ async function getUserById(userId) {
         throw error;
     }
 }
+
+async function getUserByUsername(username) {
+    try {
+      const { rows: [user] } = await client.query(`
+        SELECT *
+        FROM users
+        WHERE username=$1;
+      `, [username]);
+  
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
 
 async function createPost({
     authorId,
@@ -180,6 +193,13 @@ async function getPostById(postId) {
             FROM posts
             WHERE id=$1;
         `, [postId]);
+
+        if (!post) {
+            throw {
+              name: "PostNotFoundError",
+              message: "Could not find a post with that postId"
+            };
+          }
 
         const { rows: tags } = await client.query(`
             SELECT tags.*
@@ -300,6 +320,7 @@ module.exports = {
     updateUser,
     getAllUsers,
     getUserById,
+    getUserByUsername,
     createPost,
     updatePost,
     getAllPosts,
